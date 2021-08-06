870c22a413af26f6232c2ca97f5c6d3c7832ba24
Elias Vasquez <evasq710@hotmail.com>
2021-08-06 17:55:00 -0600

    Función abrirArchivo

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from tkinter import filedialog, Tk
 
-def abrirArchivo():
+documento = None
+nombreCurso = ''
+estudiantes = []
+
+def abrirArchivo():    
+    Tk().withdraw()
+    print('--> Se ha abierto la ventana para seleccionar el archivo')
     archivo = filedialog.askopenfile(
         title = "Seleccionar archivo LFP",
         initialdir = "./",
@@ -9,10 +15,39 @@ def abrirArchivo():
             ("Todos los archivos", "*.*")
         }
     )
-    texto = archivo.read()
-    archivo.close()
-    print('Lectura exitosa')
-    return(texto)
+    try:
+        texto = archivo.read()
+        archivo.close()
+        print('--> Lectura del archivo exitosa')
+        return texto
+    except:
+        return None
 
 if __name__ == '__main__':
-    print('Commit no vacío')
+    while True:
+        print("============ MENU ============")
+        print("1. Cargar archivo")
+        print("2. Mostrar reporte en consola")
+        print("3. Exportar reporte")
+        print("4. Salir de la aplicacion")
+        print("==============================")
+        ingresado = input('Ingrese el numero de la accion que desee realizar:')
+        try:
+            opcion = int(ingresado)
+            if opcion == 1: # CARGA DE ARCHIVO
+                documento = abrirArchivo()
+                if documento is not None:
+                    print(documento)
+                else:
+                    print("--> No se selecciono ningun archivo.\n")
+            elif opcion == 2:
+                print('Caso 2\n')
+            elif opcion == 3:
+                print('Caso 3\n')
+            elif opcion == 4:
+                print('--> Gracias por usar esta aplicacion :)')
+                break
+            else:
+                print("El numero ingresado no corresponde a ninguna accion.\n")
+        except:
+            print("Lo ingresado no corresponde a ninguna accion.\n")
